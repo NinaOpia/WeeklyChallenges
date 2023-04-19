6f0886c468f8859e0de20488057da5ee435af7be
Nina <boldurfaith@gmail.com>
2023-04-19 15:47:55 -0700

    initial commit

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -104,7 +104,7 @@ namespace ChallengesWithTestsMark8
         {
             for(int i = 0; i < nums.Length; i++)
             {
-                for(int j = i + 1; j< nums.Length; j++)
+                for(int j = i + 1; j < nums.Length; j++)
                 {
                     if (nums[i] + nums[j] == targetNumber)
                     {
